3819b0a2c71bc5e89a518953cf533863ba6b437e
donggyukim <zeedoutladzz@gmail.com>
2024-01-02 16:25:35 +0900

    feat: spring security 의존성 추가

--- api-server/build.gradle.kts
+++ api-server/build.gradle.kts
@@ -10,6 +10,7 @@ tasks.getByName("jar") {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springDocsOpenApiStarterVersion")}")
 
